776119654e3a3c1fed3c0f4c7a1e836227498987
shailesh <shailesh@be0e0292-c861-4e28-8362-b93d608658ca>
2010-09-02 02:02:03 +0000

    testing optional params..
    git-svn-id: http://socialtrivia.svn.beanstalkapp.com/st/trunk@18 be0e0292-c861-4e28-8362-b93d608658ca

--- stservice/src/com/graphsfm/stservice/resources/TestResource.java
+++ stservice/src/com/graphsfm/stservice/resources/TestResource.java
@@ -1,15 +1,17 @@
 package com.graphsfm.stservice.resources;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
-@Path("/sayhello/{username}")
+@Path("/sayhello")
 public class TestResource {
 	@GET
-	@Produces({"application/json", "application/xml"})
-	public Greeting getGreeting(@PathParam("username") String username) {
+	@Produces({ "application/json", "application/xml", "text/plain" })
+	public Greeting getGreeting(
+			@QueryParam("username") @DefaultValue("world") String username) {
 		Greeting g = new Greeting();
 		g.setText("Hello");
 		g.setRecipient(username);
