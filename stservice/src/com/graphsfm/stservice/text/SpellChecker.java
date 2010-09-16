0e87aa821e79f1265161a1b133627d5b16214b58
shailesh <shailesh@be0e0292-c861-4e28-8362-b93d608658ca>
2010-09-16 18:48:50 +0000

    A simple spelling corrector as described in Peter Norvig's article.
    git-svn-id: http://socialtrivia.svn.beanstalkapp.com/st/trunk@28 be0e0292-c861-4e28-8362-b93d608658ca

---
+++ stservice/src/com/graphsfm/stservice/text/SpellChecker.java
@@ -0,0 +1,184 @@
+package com.graphsfm.stservice.text;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SpellChecker {
+	private static Logger log = Logger.getLogger(SpellChecker.class.getName());
+	private HashMap<String,Integer> words = new HashMap<String, Integer>();
+	
+	public SpellChecker() {
+	}
+	
+	private void countWordFrequencies(String mstr, HashMap<String,Integer> model) {
+		String[] tokens = mstr.split("[^a-zA-Z]+");
+		for (String t : tokens) {
+			t = t.toLowerCase();
+			Integer count = model.get(t);
+			if (count == null)
+				count = 1;
+			else
+				count = count + 1;
+			model.put(t, count);
+		}
+	}
+	
+	public void init(String...fnames) {
+		for (String fname : fnames) {
+			try {
+				File f = new File(fname);
+				if (f.exists() && f.length() > 0) {
+					byte[] buffer = new byte[(int) f.length()];
+					FileInputStream is = new FileInputStream(f);
+					is.read(buffer);
+					String fstr = new String(buffer);
+					countWordFrequencies(fstr, words);
+				}
+			} catch (FileNotFoundException e) {
+				log.log(Level.WARNING, "File not found: " + fname, e);
+			} catch (IOException e) {
+				log.log(Level.WARNING, "Error: " + fname, e);
+			}
+		}
+	}
+	
+	protected void debugDump() {
+		for (Map.Entry<String, Integer> e : words.entrySet()) {
+			System.out.println(e.getKey() + " " + e.getValue());
+		}
+	}
+	
+	private void mutate1(HashSet<String> mset, String str) {
+		for (int i = 0; i < str.length(); i++) {
+			mset.add(str.substring(0, i) + str.substring(i+1)); // delete
+			if (i != str.length() - 1)
+				mset.add(str.substring(0, i) + str.charAt(i+1) + str.charAt(i) + str.substring(i+2)); // transpose
+			for (char c = 'a'; c <= 'z'; c++) {
+				mset.add(str.substring(0, i) + c + str.substring(i+1)); // substitute
+				mset.add(str.substring(0, i) + c + str.substring(i)); // insert
+			}
+		}
+		for (char c = 'a'; c <= 'z'; c++) {
+			mset.add(str + c); // insert
+		}
+	}
+	
+	private void includeKnownWords(HashSet<String> mset, String candidate, HashMap<String,Integer>...models) {
+		for (HashMap<String,Integer> model : models) {
+			if (model.containsKey(candidate)) {
+				mset.add(candidate);
+				return;
+			}
+		}
+	}
+	
+	private void mutate2(HashSet<String> mset, String str, HashMap<String,Integer>...models) {
+		for (int i = 0; i < str.length(); i++) {
+			String mutation = str.substring(0, i) + str.substring(i+1); // delete
+			includeKnownWords(mset, mutation, models);
+			if (i != str.length() - 1) {
+				mutation = str.substring(0, i) + str.charAt(i+1) + str.charAt(i) + str.substring(i+2); // transpose
+				includeKnownWords(mset, mutation, models);
+			}
+			for (char c = 'a'; c <= 'z'; c++) {
+				mutation = str.substring(0, i) + c + str.substring(i+1); // substitute
+				includeKnownWords(mset, mutation, models);
+				
+				mutation = str.substring(0, i) + c + str.substring(i); // insert
+				includeKnownWords(mset, mutation, models);
+			}
+		}
+		for (char c = 'a'; c <= 'z'; c++) {
+			includeKnownWords(mset, str + c, models); // insert
+		}
+	}
+	
+	public String correct(String in, HashMap<String,Integer>...models) {
+		for (HashMap<String, Integer> model : models) {
+			if (model.containsKey(in))
+				return in;
+		}
+		
+		HashSet<String> m2set = null;
+		HashSet<String> m1set = new HashSet<String>();
+		mutate1(m1set, in);
+		String ret = null;
+		int score = 0;
+		for (HashMap<String,Integer> model : models) {
+			for (String candidate : m1set) {
+				Integer candidateScore = model.get(candidate);
+				if (candidateScore != null && candidateScore > score) {
+					ret = candidate;
+					score = candidateScore;
+				}
+			}
+			if (ret != null)
+				return ret;
+			
+			if (m2set == null) {
+				m2set = new HashSet<String>();
+				for (String m1str : m1set) {
+					mutate2(m2set, m1str, models);
+				}
+			}
+			
+			for (String candidate : m2set) {
+				Integer candidateScore = model.get(candidate);
+				if (candidateScore != null && candidateScore > score) {
+					ret = candidate;
+					score = candidateScore;
+				}
+			}
+			if (ret != null)
+				return ret;
+		}
+		return in;
+	}
+	
+	public String correct(String in) {
+		return correct(in, words);
+	}
+	
+//	public String correct(String[] localText, String in) {
+//		HashMap<String,Integer> localModel = new HashMap<String, Integer>();
+//		if (localText != null) {
+//			for (String str : localText) {
+//				countWordFrequencies(str, localModel);
+//			}
+//		}
+//		
+//		StringBuilder sb = new StringBuilder();
+//		in = in.toLowerCase();
+//		String[] iwords = in.split("\\s+");
+//		for (String iword : iwords) {
+//			if (localModel.containsKey(iword)) {
+//				if (sb.length() > 0)
+//					sb.append(' ');
+//				sb.append(iword);
+//			}
+//		}
+//		return in;
+//	}
+//	
+	public static void main(String...args) throws Exception {
+		SpellChecker m = new SpellChecker();
+		m.init(args[0]);
+		
+		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			String s = r.readLine();
+			if (s == null)
+				break;
+			System.out.println(m.correct(s));
+		}
+	}
+}
