8827dcaf2a026b2fa90e053d6f7e2e112caf8690
Shailesh Bavadekar <shailesh.bavadekar@gmail.com>
2010-11-01 09:22:38 -0700

    fixed eclipse build.

--- stservice/src/com/graphsfm/stservice/text/WikiExtractor.java
+++ stservice/src/com/graphsfm/stservice/text/WikiExtractor.java
@@ -2,7 +2,6 @@ package com.graphsfm.stservice.text;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.regex.Matcher;
