c1e9bff03ee3bbbe6ad594e6aec8b48a57710244
Shailesh Bavadekar <shailesh.bavadekar@gmail.com>
2011-03-13 11:20:46 -0700

    more clustering stuff

--- experimental/cluster.py
+++ experimental/cluster.py
@@ -4,21 +4,26 @@ import random
 
 def cluster(data, nclusters):
     cids=[0 for i in range(0,len(data))]
-    guess=range(0,nclusters)
-    #guess=data[0:nclusters]
+
+    centroids=data[0:nclusters]
+
     done=0
     while not done:
         done=1
         for i in range(0, len(data)):
-            mindist=2**31-1
+            mindist=sys.maxint
             closest=-1
             for j in range(0, nclusters):
-                if abs(data[i]-guess[j]) < mindist:
-                    mindist = abs(data[i]-guess[j])
+                if abs(data[i]-centroids[j]) < mindist:
+                    mindist = abs(data[i]-centroids[j])
                     closest=j
             if cids[i] != closest:
                 cids[i]=closest
                 done=0
+
+        print "####################################################"
+        print "centroids: " + str(centroids)
+        dump_clusters(nclusters, data, cids)
 
         if not done:
             newguess=[0 for i in range(0, nclusters)]
@@ -29,28 +34,30 @@ def cluster(data, nclusters):
             for j in range(0, nclusters):
                 if newcounts[j] !=0:
                     newguess[j] = 1.0*newguess[j]/newcounts[j]
-            guess=newguess
-    print "cluster centers: " + str(guess)
+            centroids=newguess
+    print "cluster centers: " + str(centroids)
     return cids
 
+
+def dump_clusters(nclusters, data, cids):
+    for i in range(0,nclusters):
+        tlist=[]
+        for j in range(0, len(data)):
+            if i == cids[j]:
+                tlist.append(data[j])
+        tlist.sort()
+        print tlist
 
 nclusters=4
 data=[]
 
-for i in range(0,10):
-    for j in range(0, nclusters):
-        data.append(1000 * j + random.randint(0,50))
+for j in range(0, nclusters):
+    for i in range(0,10):
+        data.append(100 * j + random.randint(0,20))
 
+print "data: " + str(data)
+
+nclusters=5
 cids=cluster(data, nclusters)
-
-#print data
-#print
-for i in range(0,nclusters):
-    tlist=[]
-    for j in range(0, len(data)):
-        if i == cids[j]:
-            tlist.append(data[j])
-    tlist.sort()
-    print tlist
-
+dump_clusters(nclusters, data, cids)
 
