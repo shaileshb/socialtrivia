a5afcb8ee11702f27daf2abb39c2847279dcd2fe
meera bavadekar <meerab@yahoo.com>
2011-05-18 13:34:50 -0700

    Added game layout, play buttons and JSON/Rest piece.

---
+++ android/src/com/graphsfm/android/MediaClip.java
@@ -0,0 +1,26 @@
+package com.graphsfm.android;
+
+public class MediaClip  {
+	private String location;
+	private String bandName;
+	private String songName;
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getBandName() {
+		return bandName;
+	}
+	public void setBandName(String bandName) {
+		this.bandName = bandName;
+	}
+	public String getSongName() {
+		return songName;
+	}
+	public void setSongName(String songName) {
+		this.songName = songName;
+	}
+	
+}
---
+++ android/src/com/graphsfm/android/RestClient.java
@@ -0,0 +1,103 @@
+package com.graphsfm.android;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+ 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+ 
+import android.util.Log;
+ //earbuzilla
+
+public class RestClient {
+ 
+    private static String convertStreamToString(InputStream is) {
+        /*
+         * To convert the InputStream to String we use the BufferedReader.readLine()
+         * method. We iterate until the BufferedReader return null which means
+         * there's no more data to read. Each line will appended to a StringBuilder
+         * and returned as String.
+         */
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+ 
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+ 
+    public static JSONObject connect(String url) throws Exception
+    {
+ 
+        HttpClient httpclient = new DefaultHttpClient();
+ 
+        // Prepare a request object
+        HttpGet httpget = new HttpGet(url); 
+ 
+        // Execute the request
+        HttpResponse response;
+        try {
+            response = httpclient.execute(httpget);
+            // Examine the response status
+            Log.i("earbuzilla",response.getStatusLine().toString());
+ 
+            // Get hold of the response entity
+            HttpEntity entity = response.getEntity();
+            // If the response does not enclose an entity, there is no need
+            // to worry about connection release
+ 
+            if (entity != null) {
+ 
+                // A Simple JSON Response Read
+                InputStream instream = entity.getContent();
+                String result= convertStreamToString(instream);
+                Log.i("earbuzilla",result);
+ 
+                // A Simple JSONObject Creation
+                JSONObject json=new JSONObject(result);
+ 
+                // Closing the input stream will trigger connection release
+                instream.close();
+                return json;
+            }
+ 
+ 
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            throw e;
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            throw e;
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            throw e;
+        }
+        throw new Exception();
+    }
+ 
+}
