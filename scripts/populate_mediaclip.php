00ee8ddf452ed412942a56608fcea24c4e247f4c
meera bavadekar <meerab@yahoo.com>
2011-05-20 22:34:28 -0700

    Logic to parse the youtube html code to get fmt url.

--- scripts/populate_mediaclip.php
+++ scripts/populate_mediaclip.php
@@ -1,29 +1,51 @@
-
 <?php
-$con = mysql_connect("localhost","root","ma");
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
 
-// some code
-mysql_select_db("mysql", $con);
+$filename = "foo2.html";
+$handle = fopen($filename, "r");
+$file = fread($handle, filesize($filename));
 
-$sql_select = "SELECT * FROM media_clip_temp";
+preg_match('/flashvars="(.*)"/', $file, $matches);
+$flashvars=$matches[1];
 
-$result = mysql_query($sql_select);
+$nv_pairs=split(';', $flashvars);
 
-while($row = mysql_fetch_array($result))
-  {
-  echo 'youtube_url:   ' . $row['youtube_url'];
-
-  echo 'start:  ' . $row['startposition'];
-  $str = 'curl ' . '\''.$row['flvurl'] .'\'';
-   
-  shell_exec($str);
+foreach ($nv_pairs as $nv_pair) {
+  $nv=split('=', $nv_pair);
+  // $nv[0] is name e.g. fmt_url_map
+  // $nv[1] is value
   
+   
+  if (strcmp($nv[0], 'fmt_url_map') == 0) {
+   	echo 'Value is ' ;
+   	echo "\n";
+   	$fmt_url_map =  urldecode($nv[1]);
+   	
+   	
+   	$fmt_url_maps = split(',', $fmt_url_map);
+   	$result = array();
+   	
+   	foreach ($fmt_url_maps as $fmt_r_map) {
+   	    //echo $fmt_r_map;
+   		echo "\n";    
+   		
+   		// Now split the fmt_url_map into 
+   		
+	   	$resolution = explode('|', $fmt_r_map);
+	   	//print_r( $resolution);
+	   	array_push($result, $resolution[1]);
+	   		   			
+   	}  
+	echo "\n";     
   }
+}
 
 
-mysql_close();
-?> 
+
+print_r($result);
+
+//$wgetresult = system('wget -O mv.flv' . '"' . $result[0] . '"');
+
+//echo $wgetresult;
+
+?>
+
